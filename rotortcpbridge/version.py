a20f76575d4f3301423928b9456a03abc443db26
Joerg Koerner DK8DE <joerg@dk8.de>
2026-07-01 00:20:31 +0200

    Increment version to 1.9.7 and update release date to 01.07.2026 to reflect upcoming changes and improvements.

--- rotortcpbridge/version.py
+++ rotortcpbridge/version.py
@@ -1,7 +1,7 @@
 """Versions- und Metadaten für RotorTcpBridge."""
 
 APP_NAME = "RotorTcpBridge"
-APP_VERSION = "1.9.6"
-APP_DATE = "30.06.2026"
+APP_VERSION = "1.9.7"
+APP_DATE = "01.07.2026"
 APP_AUTHOR = "Jörg Körner DK8DE"
 APP_COPYRIGHT = "2026 Jörg Körner DK8DE"

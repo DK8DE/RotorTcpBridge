99bf1b43d33dead57897437b811d66e6aa589b9e
Joerg Koerner DK8DE <joerg@dk8.de>
2026-09-04 22:12:06 +0200

    Update version to 2.0.2 and set release date to 04.09.2026 in version.py

--- rotortcpbridge/version.py
+++ rotortcpbridge/version.py
@@ -1,7 +1,7 @@
 """Versions- und Metadaten für RotorTcpBridge."""
 
 APP_NAME = "RotorTcpBridge"
-APP_VERSION = "2.0.1"
-APP_DATE = "23.08.2026"
+APP_VERSION = "2.0.2   "
+APP_DATE = "04.09.2026"
 APP_AUTHOR = "Jörg Körner DK8DE"
 APP_COPYRIGHT = "2026 Jörg Körner DK8DE"
